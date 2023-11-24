416759c5e6a9a3580a2eea991ab94be886ce04cd
scaramanga57 <brunowo@web.de>
2023-11-24 14:35:37 +0100

    added cors

--- stakingTaxMain.py
+++ stakingTaxMain.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 
 @app.route('/receiveStakingInfo', methods=['POST'])
 def receiveStakingInfo():
